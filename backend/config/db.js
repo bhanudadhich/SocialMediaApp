cfa2cd4701cabc124df47aae0fbaee9c87a00db0
bhanudadhich <bhanudadhich107@gmail.com>
2026-05-30 12:23:03 +0530

    done

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,15 @@
 import mongoose from "mongoose";
 
-const connectDb =async ()=>{
-    try {
-        await mongoose.connect(process.env.MONGODB_URL)
-        console.log("db connected")
-    } catch (error) {
-        console.log("db error")
-    }
-}
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      serverSelectionTimeoutMS: 5000,
+      connectTimeoutMS: 5000,
+    });
+    console.log("db connected");
+  } catch (error) {
+    console.log("db error:", error.message);
+  }
+};
 
-export default connectDb
+export default connectDb;
